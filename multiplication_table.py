212a724332d41de366332078e335e512216ff956
jinu <copperwhisky15@gmail.com>
2026-09-16 21:04:53 +0530

    Add multiplication table for 3 and sum calculation
    The script generates a multiplication table for the number 3 and calculates the sum of the first 11 natural numbers.

---
+++ multiplication_table.py
@@ -0,0 +1,13 @@
+# Output Result: python table output.jpg
+
+
+
+    ## 3 table
+for i in range(3,4):
+    for j in range(1,11):
+        print(f"{i} x{j}={i*j}")
+
+sum_numbers = 0
+for k in range(1,12):
+    sum_numbers += k
+    print(f"sum of first 11 numbers is= {sum_numbers}")
